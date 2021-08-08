bb571a4ed6e6cf48e0b3e7590d6f68438245b68f
kokoa0429 <kokoa@ciebus.net>
2021-08-08 12:29:48 +0900

    add: Logger

--- main.js
+++ main.js
@@ -10,19 +10,42 @@ const server = app.listen(3000, function () {
 
 app.post("/", (req, res) => {
     if(!req.query.o) {
+        info("POST j2e")
         res.send(j2e(req.body))
     }
     else {
+        info("POST j2o")
         res.send(req.body[req.query.o] + "")
     }
 })
 
 app.get("/", (req, res) => {
     if (!req.query.url || !req.query.token !== process.env.token) {
+        error("GET ERR")
         res.send("error")
     } else {
+        error("GET")
         axios.get(req.query.url).then(function (response) {
             res.send(j2e(response.data))
         })
     }
 })
+
+function getLoggerTime() {
+    const now = new Date()
+    const date = [now.getFullYear(),z(now.getMonth()),z(now.getDate())].join("/")
+    const time = [z(now.getHours()),z(now.getMinutes()),z(now.getSeconds())].join(":")
+    return "[" + date + " " + time + "]"
+}
+
+function info(msg) {
+    console.log("[INFO]" + getLoggerTime() + " " + msg)
+}
+
+function error(msg) {
+    console.log("[EROR]" + getLoggerTime() + " " + msg)
+}
+
+function z(num){
+    return ('00' + num).slice(-2);
+}
